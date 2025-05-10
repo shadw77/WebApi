eef61ac36492e12823e99ede9cc5f83484effb66
Shadwa Mohamed <shadelhady@gmail.com>
2025-05-10 10:52:23 +0300

    Add project files.

---
+++ WebApiDemo/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Range(1000, 10000)]
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WebApiDemo/Models/ProjectEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiDemo.Models
+{
+    public class ProjectEntity:DbContext
+    {
+        public ProjectEntity() 
+        {
+
+        }
+        public ProjectEntity(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MyDatabase;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
